616939999df0c1fc644cbf6c5689f04387a66343
NCanhhieu <109597988+NCanhhieu@users.noreply.github.com>
2022-10-26 18:35:54 +0700

    Update SQLQuery9.sql

--- SQLQuery9.sql
+++ SQLQuery9.sql
@@ -17,23 +17,18 @@ Code varchar(20) primary key,
 Name nvarchar(255) not null,
 Descrip ntext ,
 CategoryID int foreign key references  Asm6_Categorys(Id) not null,
+Publishyear int not null check(Publishyear <= 2022 and Publishyear > 0 ),
+NumberofPublish int not null check(NumberofPublish > 0) default 1,
+Price Decimal(12,4) not null check(Price >= 0) default 0,
+Qty int not null check(Qty >= 0) default 0,
+PublisherID int foreign key references  Asm6_Publishers(Id) not null,
 );
 
 create table  Asm6_AuthorBooks(
 AuthorID int foreign key references  Asm6_Authors(Id) not null,
 BookCode nvarchar(20) foreign key references  Asm6_Books(Code) not null,
 );
-declare @x int;
-set @x= ( select convert(int,year(getdate())) );
 
-create table  Asm6_BookPublishs(
-Publishyear int not null check(Publishyear <= @x ),
-NumberofPublish int not null check(NumberofPublish > 0) default 1,
-Price Decimal(12,4) not null check(Price >= 0) default 0,
-Qty int not null check(Qty >= 0) default 0,
-PublisherID int foreign key references  Asm6_Publishers(Id) not null,
-BookID varchar(20) foreign key references  Asm6_Books(code) not null
-);
 
 --2. Viết lệnh SQL chèn vào các bảng của CSDL các dữ liệu mẫu
 
@@ -47,77 +42,70 @@ values(N'Eran Katz');
 insert into  Asm6_Publishers(Name,Address)
 values(N'Tri Thức', N'Nhà xuất bản 53 Nguyễn Du, Hai Bà Trưng, Hà Nội');
 
-insert into Asm6_Books(Code,Name,Descrip,CategoryID)
+insert into Asm6_Books(Code,Name,Descrip,CategoryID,Publishyear,NumberofPublish,Price,Qty,PublisherID)
 values('B001',N'Trí tuệ Do Thái',N'Bạn có muốn biết: Người Do Thái sáng tạo ra cái gì và nguồn gốc
 trí tuệ của họ xuất phát từ đâu không? Cuốn sách này sẽ dần hé lộ
 những bí ẩn về sự thông thái của người Do Thái, của một dân tộc
 thông tuệ với những phương pháp và kỹ thuật phát triển tầng lớp trí
 thức đã được giữ kín hàng nghìn năm như một bí ẩn mật mang tính
-văn hóa.', 1);
+văn hóa.', 1 ,2010,1,79000,100,1);
 
 insert into  Asm6_AuthorBooks(AuthorID,BookCode)
 values (1,'B001');
 
-insert into   Asm6_BookPublishs(Publishyear,NumberofPublish,Price,Qty,PublisherID,BookID)
-values (2010,1,79000,100,1,'B001');
-
 
 --3. Liệt kê các cuốn sách có năm xuất bản từ 2008 đến nay
-select * from Asm6_Books where  Code in 
-(select BookID from Asm6_BookPublishs where Publishyear >= 2008);
+select * from Asm6_Books  where Publishyear >= 2008 ;
 --4. Liệt kê 10 cuốn sách có giá bán cao nhất
-select  * from Asm6_Books where Code in 
-(select TOP(10) BookID from Asm6_BookPublishs Order by Price desc )
+select TOP(10)  * from Asm6_Books  Order by Price desc  
 --5. Tìm những cuốn sách có tiêu đề chứa từ “tin học”
 select * from Asm6_Books where Name like N'%tin học%'  
 --6. Liệt kê các cuốn sách có tên bắt đầu với chữ “T” theo thứ tự giá giảm dần
-select * from Asm6_Books where Name like N'T%'  Code in 
-(select  BookID from Asm6_BookPublishs Order by Price desc )
+select * from Asm6_Books where Name like N'T%'   Order by Price desc 
 --7. Liệt kê các cuốn sách của nhà xuất bản Tri thức
-select * from Asm6_Books where Name like N'T%'  Code in 
-(select  BookID from Asm6_BookPublishs where PublisherID in
- (select Id from Asm6_Publishers where Name like 'Tri Thức' ))
+select * from Asm6_Books  where PublisherID in
+ (select Id from Asm6_Publishers where Name like N'Tri Thức' )
 --8. Lấy thông tin chi tiết về nhà xuất bản xuất bản cuốn sách “Trí tuệ Do Thái”
 select * from Asm6_Publishers where Id in
- (select PublisherID from Asm6_BookPublishs where BookID in 
-   (select Code from Asm6_Books where Name like 'Trí tuệ Do Thái'))
+ (select PublisherID  from Asm6_Books where Name like 'Trí tuệ Do Thái')
 --9. Hiển thị các thông tin sau về các cuốn sách: Mã sách, Tên sách, Năm xuất bản, Nhà xuất bản,Loại sách
 select A.Code, A.Name, B.Publishyear, C.Name, D.Name  from Asm6_Books A
-  inner join Asm6_BookPublishs B on A.Code = B.BookID
-  inner join Asm6_Publishers C on B.PublisherID = C.Id
-  inner join Asm6_Categorys D on A.CategoryID = D.Id
+  left join Asm6_Publishers B on A.PublisherID = B.Id
+  left join Asm6_Categorys C on A.CategoryID = C.Id
 --10. Tìm cuốn sách có giá bán đắt nhất
-select  * from Asm6_Books where Code in 
-(select TOP(1) BookID from Asm6_BookPublishs Order by Price desc )
+select TOP(1) * from Asm6_Books Order by Price desc 
 --11. Tìm cuốn sách có số lượng lớn nhất trong kho
-select  * from Asm6_Books where Code in 
-(select TOP(1) BookID from Asm6_BookPublishs Order by Qty desc )
+select TOP(1) * from Asm6_Books  Order by Qty desc )
 --12. Tìm các cuốn sách của tác giả “Eran Katz”
 select * from Asm6_Books where   Code in 
 (select  BookID from Asm6_AuthorBooks where AuthorID in 
  (select Id from Asm6_Authors where Name like 'Eran Katz'))
 
 --13. Giảm giá bán 10% các cuốn sách xuất bản từ năm 2008 trở về trước
-update Asm6_BookPublishs set Price = Price*(90.0/100) where Publishyear  <= 2008 ;
+update Asm6_Books set Price = Price*(90.0/100) where Publishyear  <= 2008 ;
 --14. Thống kê số đầu sách của mỗi nhà xuất bản
-select   PublisherID,Count(BookID) as So_dau_sach from Asm6_BookPublishs Group by PublisherID
+select   PublisherID,Count(*) as So_dau_sach from Asm6_Books Group by PublisherID
+--cách 2
+select * from Asm6_Publishers a left join
+(select   PublisherID,Count(*) as So_dau_sach from Asm6_Books Group by PublisherID) b on a.Id = b.PublisherID
 --15. Thống kê số đầu sách của mỗi loại sách
 select CategoryID, Count(Name) from Asm6_Books group by CategoryID 
 --16. Đặt chỉ mục (Index) cho trường tên sách
 create index Book_name on Asm6_Books(Name)
 --17. Viết view lấy thông tin gồm: Mã sách, tên sách, tác giả, nhà xb và giá bán
 create view Book_Data as
-select A.Code, A.Name, E.Name , C.Name, B.Price  from Asm6_Books A
-  inner join Asm6_BookPublishs B on A.Code = B.BookID
-  left join Asm6_Publishers C on B.PublisherID = C.Id
+select A.Code, A.Name, E.Name  as authorname, C.Name as pubName, A.Price  from Asm6_Books A
+  left join Asm6_Publishers C on A.PublisherID = C.Id
   left join Asm6_AuthorBooks D on A.Code = D.BookCode
   left join Asm6_Authors E on D.AuthorID = E.Id
 
 --18. Viết Store Procedure:
 --SP_Them_Sach: thêm mới một cuốn sách
-create procedure SP_Them_Sach @Code varchar(20), @Name nvarchar(255),@Descrip ntext ,@CategoryID int as
-insert into Asm6_Books(Code,Name,Descrip,CategoryID)
-values(@Code,@Name,@Descrip,@CategoryID)
+create procedure SP_Them_Sach @Code varchar(20), @Name nvarchar(255),@Descrip ntext ,@CategoryID int 
+@Publishyear int, @NumberofPublish int , @Price Decimal(12,4),@Qty int
+ as
+insert into Asm6_Books(Code,Name,Descrip,CategoryID,Publishyear,NumberofPublish,Price,Qty )
+values(@Code,@Name,@Descrip,@CategoryID,@Publishyear,@NumberofPublish,@Price,@Qty)
 --SP_Tim_Sach: Tìm các cuốn sách theo từ khóa
 create procedure SP_Tim_Sach @Name nvarchar(255) as
 select * from Asm6_Books where Name like @Name;
